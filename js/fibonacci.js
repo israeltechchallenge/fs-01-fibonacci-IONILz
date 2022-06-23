5e28ba46ec2bc75739ee9df29e2f0042133c2c03
IONILz <awiwmaman@outlook.com>
2022-06-23 15:19:55 +0300

    Milestone 2

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,8 +1,35 @@
 const elementOfIndex = document.getElementById("num1");
 const elementOfResult = document.getElementById("num2");
+let chosenFibonacci = 13;
 
-let fibonacci = 7;
-let resultOfFibonacci = 8;
+// function getFibonacci(x) {
+//     if (x === 0) {
+//         return 0;
+//     } else if (x === 1) {
+//         return 1;
+//     } else {
+//         let arrOfFibonacci = [0, 1];
+//         for (let index = 0; index < x - 1; index++) {
+//             arrOfFibonacci[arrOfFibonacci.length] = arrOfFibonacci[arrOfFibonacci.length - 2] + arrOfFibonacci[arrOfFibonacci.length - 1];
+//         }
+//         return arrOfFibonacci[x];
+//     }
+// };
 
-elementOfIndex.innerText = fibonacci;
-elementOfResult.innerText = resultOfFibonacci;
+function getFibonacci(x) {
+    if (x <= 0) {
+        return 0;
+    } else if (x === 1) {
+        return 1;
+    } else {
+        let arrOfFibonacci = [0, 1];
+        for (let index = 2; index < x + 2; index++) {
+            arrOfFibonacci[index] = arrOfFibonacci[index - 2] + arrOfFibonacci[index - 1];
+        }
+        // Result of Fibonacci = arrOfFibonacci[x] = y
+        return arrOfFibonacci[x];
+    }
+};
+
+elementOfIndex.innerText = chosenFibonacci;
+elementOfResult.innerText = getFibonacci(chosenFibonacci);

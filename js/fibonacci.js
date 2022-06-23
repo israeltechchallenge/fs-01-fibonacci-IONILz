cbac8338cd9c2c878f876166f5cf48a7c4ee5d5e
IONILz <awiwmaman@outlook.com>
2022-06-23 15:53:14 +0300

    Milestone 4

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,30 +1,22 @@
 const elementOfIndex = document.getElementById("num1");
 const calculateButton = document.getElementById("btn1");
 const elementOfResult = document.getElementById("num2");
-let chosenFibonacci;
 
-elementOfIndex.addEventListener("input", changeFibonacciIndex);
 calculateButton.addEventListener("click", putYOfFibonacci);
 
-function getFibonacci(x) {
-    if (x <= 0) {
-        return 0;
-    } else if (x === 1) {
-        return 1;
-    } else {
-        let arrOfFibonacci = [0, 1];
-        for (let index = 2; index < x + 2; index++) {
-            arrOfFibonacci[index] = arrOfFibonacci[index - 2] + arrOfFibonacci[index - 1];
-        }
-        // Result of Fibonacci = arrOfFibonacci[x] = y
-        return arrOfFibonacci[x];
-    }
-};
-
-function changeFibonacciIndex() {
-    chosenFibonacci = elementOfIndex.value;
-};
-
 function putYOfFibonacci() {
-    elementOfResult.innerHTML = `<u><b>${getFibonacci(chosenFibonacci)}</b></u>`;
+    elementOfResult.innerHTML = `<u><b>${getFibonacciFromServer(elementOfIndex.value)}</b></u>`;
 };
+
+function getFibonacciFromServer(num) {
+    let FIBONACCI_URL = `http://localhost:5050/fibonacci/${num}`;
+    fetch(FIBONACCI_URL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            elementOfResult.innerHTML = `<u><b>${data.result}</b></u>`;
+        });
+};
+
+

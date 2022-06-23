9677da94e3f84cdcc9c6945f47a20709d85e55c3
IONILz <awiwmaman@outlook.com>
2022-06-23 22:26:00 +0300

    Milestone 5

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,22 +1,38 @@
 const elementOfIndex = document.getElementById("num1");
 const calculateButton = document.getElementById("btn1");
 const elementOfResult = document.getElementById("num2");
+const loadingSpinner = document.getElementById("spinner");
 
 calculateButton.addEventListener("click", putYOfFibonacci);
 
 function putYOfFibonacci() {
-    elementOfResult.innerHTML = `<u><b>${getFibonacciFromServer(elementOfIndex.value)}</b></u>`;
+    elementOfIndex.classList.remove("is-invalid");
+    elementOfIndex.classList.remove("text-danger");
+    elementOfResult.classList.remove("text-danger");
+    if (elementOfIndex.value > 50) {
+        elementOfResult.innerHTML = "";
+        elementOfIndex.classList.add("is-invalid");
+        elementOfIndex.classList.add("text-danger");
+    } else {
+        elementOfResult.innerHTML = `<u><b>${getFibonacciFromServer(elementOfIndex.value)}</b></u>`;
+    }
 };
 
 function getFibonacciFromServer(num) {
+    loadingSpinner.classList.remove("visually-hidden");
     let FIBONACCI_URL = `http://localhost:5050/fibonacci/${num}`;
     fetch(FIBONACCI_URL)
         .then(function (response) {
             return response.json();
+        }).then(function (data) {
+            elementOfResult.innerHTML = `&nbsp;<u><b>${data.result}</b></u>`;
+            loadingSpinner.classList.add("visually-hidden");
+        }).catch(function (error) {
+            // elementOfResult.innerHTML = `<b>${error}</b>`;
+            elementOfResult.classList.add("text-danger");
+            elementOfResult.innerHTML = `&nbsp;Server Error: 42 is the meaning of life`;
+            loadingSpinner.classList.add("visually-hidden");
         })
-        .then(function (data) {
-            elementOfResult.innerHTML = `<u><b>${data.result}</b></u>`;
-        });
 };
 
 

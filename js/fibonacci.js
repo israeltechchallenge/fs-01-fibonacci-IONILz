6a8e6424b72048caa5058182565b9d6f9f03f0d2
IONILz <awiwmaman@outlook.com>
2022-06-23 15:15:18 +0300

    Milestone 1

---
+++ js/fibonacci.js
@@ -0,0 +1,8 @@
+const elementOfIndex = document.getElementById("num1");
+const elementOfResult = document.getElementById("num2");
+
+let fibonacci = 7;
+let resultOfFibonacci = 8;
+
+elementOfIndex.innerText = fibonacci;
+elementOfResult.innerText = resultOfFibonacci;

3e6a905e7d3935d06b0e5bc9e2fa1c9e9764dc2b
IONILz <awiwmaman@outlook.com>
2022-06-25 21:30:04 +0300

    Fixes to the code

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -19,20 +19,51 @@ function putYOfFibonacci() {
 };
 
 function getFibonacciFromServer(num) {
+    elementOfResult.innerHTML = "";
     loadingSpinner.classList.remove("visually-hidden");
+    elementOfResult.classList.remove("fs-5");
     let FIBONACCI_URL = `http://localhost:5050/fibonacci/${num}`;
     fetch(FIBONACCI_URL)
         .then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            elementOfResult.innerHTML = `&nbsp;<u><b>${data.result}</b></u>`;
-            loadingSpinner.classList.add("visually-hidden");
-        }).catch(function (error) {
-            elementOfResult.classList.add("text-danger");
-            elementOfResult.innerHTML = `&nbsp;${error}`;
-            // elementOfResult.innerHTML = `&nbsp;Server Error: 42 is the meaning of life`;
-            loadingSpinner.classList.add("visually-hidden");
+            if (response.statusText === "OK") {
+                return response.json();
+            } else {
+                elementOfResult.innerHTML = `&nbsp;Server Error: 42 is the meaning of life`;
+                elementOfResult.classList.add("fs-5");
+                throw response;
+            }
+        })
+        .then(function (data) {
+            if (data) {
+                elementOfResult.innerHTML = `&nbsp;<u><b>${data.result}</b></u>`;
+                loadingSpinner.classList.add("visually-hidden");
+            }
+        })
+        .catch(function (error) {
+            if (error) {
+                elementOfResult.classList.add("text-danger");
+                loadingSpinner.classList.add("visually-hidden");
+            }
         })
 };
 
-
+// function getFibonacciFromServer(num) {
+//     loadingSpinner.classList.remove("visually-hidden");
+//     let FIBONACCI_URL = `http://localhost:5050/fibonacci/${num}`;
+//     fetch(FIBONACCI_URL)
+//         .then(function (response) {
+//             console.log(response);
+//             if (!response.ok) {
+//                 console.log(response.value);
+//                 elementOfResult.classList.add("text-danger");
+//                 elementOfResult.innerHTML = `&nbsp;${response.statusText}`;
+//                 loadingSpinner.classList.add("visually-hidden");
+//             } else {
+//                 return response.json();
+//             }
+//         })
+//         .then(function (data) {
+//             elementOfResult.innerHTML = `&nbsp;<u><b>${data.result}</b></u>`;
+//             loadingSpinner.classList.add("visually-hidden");
+//         })
+// };
